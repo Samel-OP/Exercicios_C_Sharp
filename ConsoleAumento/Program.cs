965f4ea2ec2d66471bbbf1a32e078525553e2757
Samel <samueldeoliveirapereira1@gmail.com>
2021-04-30 16:14:55 -0300

    Exercício_3

---
+++ ConsoleAumento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva o nome do funcionário: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Escreva seu salário: ");
+            float salario = float.Parse(Console.ReadLine());
+
+            if(salario <= 500)
+            {
+                double aumento = salario + (salario * 0.3);
+                Console.WriteLine(nome + " agora seu salário é R$" + aumento);
+            }
+            else
+            {
+                Console.WriteLine(nome + ", você não tem direito a aumento");
+            }
+        }
+    }
+}
